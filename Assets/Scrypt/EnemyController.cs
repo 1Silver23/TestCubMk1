01e7f9af1383ed1ee3155ce5136313dfe155fb52
1Silver23 <SilverWoolf82@mail.ru>
2022-10-17 20:52:43 +0300

    Add files
    Add project

---
+++ Assets/Scrypt/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] waypoints; 
+
+    [SerializeField] private float moveSpeed;
+    int curWayPointIndex = 0;
+
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, waypoints[curWayPointIndex].position, Time.deltaTime * moveSpeed);
+        if (Vector3.Distance(transform.position, waypoints[curWayPointIndex].position) < 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scrypt/EnemySpawner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPosition;
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _spawnTime;
+    [SerializeField] private float _nextSpawnTime;
+    
+    [SerializeField] private int _maxEnemy = 3;
+    private int _currentEnemyNum = 0;
+
+    //private void Awake() => Invoke("SpawnEnemy", _spawnTime);
+
+    private void Awake()
+    {
+        InvokeRepeating("SpawnEnemy", _spawnTime, _nextSpawnTime);
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(_enemy, transform.position, Quaternion.identity);
+        _currentEnemyNum++;
+        if (_currentEnemyNum >= _maxEnemy)
+        {
+            CancelInvoke("SpawnEnemy");
+        }
+    }
+
+
+
+    //private void SpawnEnemy()
+    //{
+    //    //Instantiate(_enemy, transform.position, Quaterion.identity);  // из урока гиков.
+
+    //    _spawnPosition = GameObject.FindGameObjectWithTag("Enemy").transform;
+    //}
+
+    //private void Instantiate(GameObject enemy, Vector3 position, object identity)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+}
